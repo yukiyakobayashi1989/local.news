68c6756d53f892f42764e90b677a5093cec37f37
yukiya_kobayashi1989 <ykkpekuri@yahoo.co.jp>
2021-04-14 21:10:13 +0900

    20210414-1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//php10追記
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create','Admin\NewsController@add');
+  //php10課題
+  Route::get('profile/create','Admin\ProfileController@add');
+  Route::get('profile/edit','Admin\ProfileController@edit');
+});
